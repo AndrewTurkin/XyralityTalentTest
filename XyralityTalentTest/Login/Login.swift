3cf6e0b348fdf7d3a4c29c29c48048dc56e2d37e
Andrew <andrewturk30@gmail.com>
2016-02-18 21:57:47 +0200

    Created Storyboard and View Controller classes

---
+++ XyralityTalentTest/Login/Login.swift
@@ -0,0 +1,13 @@
+//
+//  Login.swift
+//  XyralityTalentTest
+//
+//  Created by Andrew Turkin on 2/18/16.
+//  Copyright © 2016 andrew.turkin. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    
+}
---
+++ XyralityTalentTest/Worlds/Worlds.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XyralityTalentTest
+//
+//  Created by Andrew Turkin on 2/18/16.
+//  Copyright © 2016 andrew.turkin. All rights reserved.
+//
+
+import UIKit
+
+class Worlds: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
